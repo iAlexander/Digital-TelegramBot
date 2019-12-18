cc1f7180755a9aad74565f5c377a6afa9e8f56de
Misha Marinenko <0x77dev@protonmail.com>
2019-12-18 20:16:40 +0200

    Backup

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Telegraf = require("telegraf");
 const config = require("./config");
 const Core = require("./lib/core");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/itop", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 const bot = new Telegraf(config.telegramBotToken);
 const core = new Core(bot);
